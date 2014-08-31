973bd0def7e65fff594b40cd78f79715b5120b72
Ryan Trinkle <ryan.trinkle@gmail.com>
2014-08-31 14:02:10 -0400

    Add ghcjs.nix with explanation

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ghcjs-setup
 ===========
 
-Tools for getting started with ghcjs
+```bash
+git clone https://github.com/ryantrinkle/ghcjs-setup
+cd ghcjs-setup
+./ghcjs-nix.sh
+```
+
+This will install the Nix package manager, grab the ghcjs package information, build everything, then give you access to a sandbox with ghcjs and node.js available.  The first time you run it, the build process will take a long time - up to several hours.  After that, it should only take a few moments.
+
+You will need sudo access in order to install Nix (unless you are on NixOS).  However, you should run the script as yourself, not root - it will prompt for your password when necessary.
---
+++ ghcjs-nix.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -eu
+
+trap "echo 'It looks like a problem occurred.  Please submit an issue at https://github.com/ryantrinkle/ghcjs-setup/issues'; exit 1" ERR
+
+echo "If you have any trouble with this script, please submit an issue at https://github.com/ryantrinkle/ghcjs-setup/issues"
+
+if [ ! -d /nix ] ; then
+  if ! type -P curl ; then
+    echo "Please make sure that 'curl' is installed and can be run from this shell"
+    exit 1
+  fi
+
+  echo "Installing Nix"
+  bash <(curl https://nixos.org/nix/install)
+fi
+
+if ! type -P nix-shell ; then
+  . ~/.nix-profile/etc/profile.d/nix.sh
+fi
+
+if [ ! -d nixpkgs ] ; then
+  if ! type -P git ; then
+    echo "Please make sure that 'git' is installed and can be run from this shell"
+    exit 1
+  fi
+
+  echo "Getting ghcjs build scripts"
+  git clone https://github.com/ryantrinkle/nixpkgs
+fi
+
+INFO=$(cat <<"EOF"
+
+You are now in a shell with access to ghcjs.  You can verify that ghcjs is configured correctly with:
+$ ghcjs --version
+The Glorious Glasgow Haskell Compilation System for JavaScript, version 0.1.0 (GHC 7.8.3)
+
+To build a basic program with ghcjs, do:
+$ echo 'main = putStrLn "Hello, world!"' > helloWorld.hs
+$ ghcjs helloWorld.hs
+$ node helloWorld.jsexe/all.js
+EOF
+)
+
+echo "Entering the ghcjs/nodejs sandbox; this may take several hours the first time you run it"
+nix-shell -I . -p haskellPackages_ghcjs.ghc nodejs --command "echo \"$INFO\" ; return"
