72b2161a58fb97711e02bd3c8f88ae870c7c3224
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-01-02 09:02:16 -0500

    Use cabalInstall_HEAD instead of cabalInstall, for ghcjs support

--- ghcjs-setup.sh
+++ ghcjs-setup.sh
@@ -46,4 +46,4 @@ EOF
 )
 
 echo "Entering the ghcjs/nodejs sandbox; this may take several hours the first time you run it"
-nix-shell -I . -p haskellPackages_ghcjs.ghc haskellPackages_ghcjs.ghc.ghc.parent.cabalInstall nodejs --command "echo \"$INFO\" ; return"
+nix-shell -I . -p haskellPackages_ghcjs.ghc haskellPackages_ghcjs.ghc.ghc.parent.cabalInstall_HEAD nodejs --command "echo \"$INFO\" ; return"
